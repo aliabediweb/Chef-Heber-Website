e0d87c371c6313fe1d3cb786389bac18592b2d27
Ali Abedi <aliabediweb@outlook.com>
2021-03-22 00:16:15 -0400

    first push

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Chef Heber Shopping List',
+        }
+    },
+    methods: {
+    //     changeTitle(title) {
+    //         this.title = title
+    //     },
+    //     toggleShowBooks() {
+    //         this.showBooks = !this.showBooks
+    //     },
+    //     handleEvent(e, data) {
+    //         console.log(e, e.type)
+    //         if (data) {
+    //             console.log(data)
+    //         }
+    //     },
+    //     handleMousemove(e) {
+    //         this.x = e.offsetX
+    //         this.y = e.offsetY
+    //         this.alt = e.altKey
+    //     },
+        createList() {
+            console.log("HI")
+        }
+    }, 
+    computed: { 
+        // filteredBooks() {
+        //     return this.books.filter((book) => book.isFav)
+        // }
+    }
+})
+
+app.mount('#app')
